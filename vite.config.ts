4f7ab78f830e283650e21b1a5f3e1da4ce669bd2
Pitt Wu <kgb00128@gmail.com>
2022-08-10 10:44:52 +0800

    chore: init

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,7 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
-import path from 'path';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  esbuild: {
-    jsxFactory: 'h',
-    jsxFragment: 'Fragment',
-  },
-  base: '/dist/',
-  build: {
-    outDir: '',
-    emptyOutDir: true,
-    manifest: true,
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-});
+  plugins: [vue()]
+})
